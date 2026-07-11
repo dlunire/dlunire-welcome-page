95ae65c4da7a85d105a62b34cbfaec08021d9632
David Eduardo Luna Montilla <dlunireframework@gmail.com>
2026-07-11 17:06:00 -0500

    Git Inicial

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import * as routing from "@dlunire/front-dlroute";
+    import Welcome from "./layouts/Svelte/Welcome.svelte";
+    import type { Component } from "svelte";
+
+    routing.route("/welcome", Welcome);
+    const dispath = routing.dispatch();
+
+    const component: Component | null = dispath.component as Component | null;
+</script>
+
+{#if dispath.validated}
+    <svelte:component this={component} />
+{/if}
