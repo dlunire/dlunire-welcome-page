20c19bbf6dfb83cd663cfd1d70ee4ff9b44d5f09
David Eduardo Luna Montilla <dlunireframework@gmail.com>
2026-07-31 09:40:22 -0500

    Update

--- src/App.svelte
+++ src/App.svelte
@@ -4,13 +4,17 @@
     import Header from "./layouts/Components/Headers/Header.svelte";
     import Footer from "./layouts/Footers/Footer.svelte";
     import { tickScroll } from "./lib/action/tick";
+    import NotFound from "./layouts/Components/NotFounds/NotFound.svelte";
 
     let dispatch: Dispatch = $state<Dispatch>(init());
     let MainComponent: Component<Record<string, any>> | undefined =
         $state<undefined>();
 
     $effect(() => {
-        if (!dispatch.validated.validated) return;
+        if (!dispatch.validated.validated) {
+            MainComponent = NotFound;
+            return;
+        }
         MainComponent = dispatch.component as Component<Record<string, any>>;
         tickScroll();
     });
@@ -20,8 +24,10 @@
     });
 </script>
 
-<Header />
-{#if MainComponent}
+{#if MainComponent === NotFound}
     <MainComponent />
+{:else}
+    <Header />
+    <MainComponent />
+    <Footer />
 {/if}
-<Footer />
